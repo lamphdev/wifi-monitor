9df97f90ac80e211bf3197a1a2e66b4dfbb9d431
LamPH <lamph11.it@gmail.com>
2022-05-15 16:40:00 +0700

    add environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,8 @@ export const environment = {
     protocol: 'ws',
     path: ''
   },
+  mqttTimeOut: 15000,
+  timoutMessage: 'Device is not responding, please try again later',
   mqttTopic: {
     GET_AP: 'vht/mesh/demo',
     GET_DEVICE: 'vht/mesh/demo'
